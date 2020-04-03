7a4265b328b2c72171df60f83a15277f06002617
DankerPhip <noah_ph@hotmail.com>
2020-04-03 18:03:09 -0400

    First commit, a fully functionaly etch-a-sketch with adjustable div sizes to allow for more precise drawings.

---
+++ etch_a_sketch.js
@@ -0,0 +1,42 @@
+let lastGridNumber = 16; 
+createNewGrid(lastGridNumber); //creates first grid with 16 squares
+
+function createNewGrid(squareNumber){
+    const container = document.querySelector("#container");
+    let gridWidth = (Math.sqrt(squareNumber));
+    for(i = 0; i < squareNumber; i++){
+        const gridSquare = document.createElement("div");
+        gridSquare.id = "div" + i;
+        gridSquare.classList.add("boxes");
+        gridSquare.style.width = ((960/gridWidth) + "px");
+        gridSquare.style.height = ((960/gridWidth) + "px");
+        gridSquare.setAttribute;
+        container.appendChild(gridSquare);
+    }
+    const boxColors = document.querySelectorAll(".boxes");
+    boxColors.forEach((div) =>{
+        div.addEventListener("mouseover", (e) => {
+            e.target.style.background = "black";
+    });
+});
+}
+
+function removeGrid(squareNumber){
+    const container = document.querySelector("#container");
+    for(i = 0; i < squareNumber; i++){
+        container.removeChild(container.lastChild);
+    }
+}
+
+const clearGrid = document.querySelector("#clear-button")
+clearGrid.addEventListener("click", function(e) {
+    let gridLength = prompt("How many boxes long would you like the grid to be?");
+    let currentGridNumber = gridLength * gridLength;
+    removeGrid(lastGridNumber);
+    createNewGrid((currentGridNumber))
+    lastGridNumber = currentGridNumber;
+}); 
+
+
+
+
